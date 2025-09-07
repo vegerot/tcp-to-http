d3205c18cab6bdc3ba88b2122ac30004b75bdf55
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2025-09-06 18:42:47 -0700

    HTTP streams

--- 1-http-streams.c
+++ 1-http-streams.c
@@ -4,17 +4,37 @@
 
 #define READ_SIZE 8
 int main() {
-    FILE *file = fopen("./messages.txt", "r");
-    if (file == NULL) {
-        perror("Failed to open the file");
-        return EXIT_FAILURE;
+  FILE *file = fopen("./messages.txt", "r");
+  if (file == NULL) {
+    perror("Failed to open the file");
+    return EXIT_FAILURE;
+  }
+  char read_buf[READ_SIZE];
+  char line_buf[READ_SIZE * 20];
+  size_t line_offset = 0;
+  line_buf[sizeof(line_buf) - 1] = 0;
+  while (true) {
+    size_t bytesread = fread(&read_buf, sizeof(char), READ_SIZE, file);
+    size_t previous_newline_pos = 0;
+    for (size_t i = 0; i < bytesread; ++i) {
+      size_t distanceFromBeginningOfReadBufOrLastNewline =
+          i - previous_newline_pos;
+      if (read_buf[i] == '\n') {
+        printf("read: %.*s\n",
+               (int)(line_offset + distanceFromBeginningOfReadBufOrLastNewline),
+               line_buf);
+        previous_newline_pos = i + 1;
+        line_offset = 0;
+        line_buf[sizeof(line_buf) - 1] = 0;
+      } else {
+        line_buf[line_offset + distanceFromBeginningOfReadBufOrLastNewline] =
+            read_buf[i];
+      }
     }
-    char buf[READ_SIZE];
-    while (true) {
-        size_t bytesread = fread(&buf, sizeof(char), READ_SIZE, file);
-        printf("%.*s", (int)bytesread, buf);
-        if (bytesread < READ_SIZE) {
-            break;
-        }
+
+    line_offset += bytesread - previous_newline_pos;
+    if (bytesread < READ_SIZE) {
+      break;
     }
+  }
 }
