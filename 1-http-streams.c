7e8ecbb83eebf9b87a0a94c4d86275914c2633f8
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2025-09-06 18:48:41 -0700

    refactor

--- 1-http-streams.c
+++ 1-http-streams.c
@@ -12,27 +12,19 @@ int main() {
   char read_buf[READ_SIZE];
   char line_buf[READ_SIZE * 20];
   size_t line_offset = 0;
-  line_buf[sizeof(line_buf) - 1] = 0;
   while (true) {
     size_t bytesread = fread(&read_buf, sizeof(char), READ_SIZE, file);
-    size_t previous_newline_pos = 0;
     for (size_t i = 0; i < bytesread; ++i) {
-      size_t distanceFromBeginningOfReadBufOrLastNewline =
-          i - previous_newline_pos;
       if (read_buf[i] == '\n') {
-        printf("read: %.*s\n",
-               (int)(line_offset + distanceFromBeginningOfReadBufOrLastNewline),
-               line_buf);
-        previous_newline_pos = i + 1;
+        line_buf[line_offset] = 0;
+        printf("read: %s\n", line_buf);
         line_offset = 0;
-        line_buf[sizeof(line_buf) - 1] = 0;
       } else {
-        line_buf[line_offset + distanceFromBeginningOfReadBufOrLastNewline] =
-            read_buf[i];
+        line_buf[line_offset] = read_buf[i];
+        line_offset += 1;
       }
     }
 
-    line_offset += bytesread - previous_newline_pos;
     if (bytesread < READ_SIZE) {
       break;
     }
