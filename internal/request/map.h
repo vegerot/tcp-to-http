198557a078f21fa451f535d02b234eacc9af717e
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2026-01-14 17:32:15 -0800

    feat: add hashmap

--- internal/request/map.h
+++ internal/request/map.h
@@ -31,6 +31,134 @@ char* mapGet(StringMap const* self, char const* key);
 
 bool mapDelete(StringMap* self, char const* key);
 
-#if defined(MAP_IMPLEMENTATION)
+#define MAP_IMPLEMENTATION
+#ifdef MAP_IMPLEMENTATION
+#include <stdlib.h>
 
+#define MAP_GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+
+static uint32_t mapHashCString(char const* key, size_t length) {
+    uint32_t hash = 2166136261u;
+    for (size_t i = 0; i < length; ++i) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619u;
+    }
+    return hash;
+}
+
+static void mapAdjustCapacity(StringMap* self, size_t newCapacity) {
+    MapEntry* newEntries = (MapEntry*)malloc(sizeof(MapEntry) * newCapacity);
+    for (size_t i = 0; i < newCapacity; ++i) {
+        newEntries[i] = (MapEntry){.type = ENTRY_EMPTY, .key = NULL, .value = NULL};
+    }
+    StringMap newSelf = {
+        .count = 0,
+        .capacity = newCapacity,
+        .entries = newEntries,
+    };
+    if (self->entries != NULL) {
+        mapAddAll(self, &newSelf);
+        free(self->entries);
+    }
+    *self = newSelf;
+}
+
+static MapEntry* mapFindEntry(StringMap const* self, char const* key, uint32_t hash) {
+    if (self->capacity == 0) {
+        assert(false && "find in uninitialized map");
+    }
+    uint32_t index = hash % self->capacity;
+    MapEntry* lastTombstone = NULL;
+    while (true) {
+        MapEntry* curr = &self->entries[index];
+        if (curr->type == ENTRY_VALUE) {
+            if (strcmp(curr->key, key) == 0) return curr;
+        } else if (curr->type == ENTRY_TOMBSTONE) {
+            if (lastTombstone == NULL) lastTombstone = curr;
+        } else if (curr->type == ENTRY_EMPTY) {
+            return lastTombstone == NULL ? curr : lastTombstone;
+        }
+        index = (index + 1) % self->capacity;
+    }
+}
+
+void mapInit(StringMap* self, size_t initialCapacity) {
+    self->count = 0;
+    self->capacity = 0;
+    self->entries = NULL;
+    if (initialCapacity > 0) {
+        mapAdjustCapacity(self, initialCapacity);
+    }
+}
+
+void mapFree(StringMap* self) {
+    if (self->entries == NULL) {
+        assert(false && "freeing uninitialized map");
+    }
+    free(self->entries);
+    self->entries = NULL;
+    self->capacity = 0;
+    self->count = 0;
+}
+
+void mapAddAll(StringMap* from, StringMap* to) {
+    if (from->count == 0) return;
+    for (size_t i = 0; i < from->capacity; ++i) {
+        MapEntry* entry = &from->entries[i];
+        if (entry->type == ENTRY_VALUE) {
+            mapSet(to, entry->key, entry->value);
+        }
+    }
+}
+
+bool mapSet(StringMap* self, char const* key, char const* value) {
+    size_t keyLen = strlen(key);
+    uint32_t hash = mapHashCString(key, keyLen);
+    if (self->count + 1 > (size_t)((int)self->capacity * MAP_MAX_LOAD_RATIO)) {
+        size_t newCapacity = MAP_GROW_CAPACITY(self->capacity);
+        mapAdjustCapacity(self, newCapacity);
+    }
+    MapEntry* entry = mapFindEntry(self, key, hash);
+    if (entry->type != ENTRY_VALUE) {
+        self->count += 1;
+    }
+    entry->type = ENTRY_VALUE;
+    entry->key = strdup(key);
+    entry->value = strdup(value);
+    return true;
+}
+
+char* mapGet(StringMap const* self, char const* key) {
+    if (self->count == 0) return NULL;
+    uint32_t hash = mapHashCString(key, strlen(key));
+    MapEntry* entry = mapFindEntry(self, key, hash);
+    if (entry->type != ENTRY_VALUE) return NULL;
+    return entry->value;
+}
+
+bool mapDelete(StringMap* self, char const* key) {
+    if (self->count == 0) return false;
+    uint32_t hash = mapHashCString(key, strlen(key));
+    MapEntry* entry = mapFindEntry(self, key, hash);
+    if (entry->type != ENTRY_VALUE) return false;
+    *entry = (MapEntry){.type = ENTRY_TOMBSTONE, .key = NULL, .value = NULL};
+    self->count -= 1;
+    return true;
+}
+
+char* mapFindString(StringMap* self, char const* chars, size_t length, uint32_t hash) {
+    if (self->count == 0) return NULL;
+    uint32_t index = hash % self->capacity;
+    while (true) {
+        MapEntry* entry = &self->entries[index];
+        if (entry->type == ENTRY_EMPTY) {
+            return NULL;
+        } else if (entry->type == ENTRY_VALUE) {
+            if (strlen(entry->key) == length && memcmp(entry->key, chars, length) == 0) {
+                return entry->key;
+            }
+        }
+        index = (index + 1) % self->capacity;
+    }
+}
 #endif
--- internal/request/map_test.c
+++ internal/request/map_test.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /// could try `__builtin_expect` if this is too slow
 #define MY_ASSERT(cond)                                                       \
@@ -96,9 +97,8 @@
 #define MAP_IMPLEMENTATION
 #include "map.h"
 
-
 static void basic(void) {
-    StringMap map = NULL;
+    StringMap map;
     mapInit(&map, 0);
     MY_ASSERT_EQUALS(map.count, 0);
 
@@ -125,7 +125,109 @@ static void basic(void) {
 	mapFree(&map);
 }
 
-int main5() {
-	basic();
-	return 0;
+static void overwrite(void) {
+    StringMap map ;
+    mapInit(&map, 0);
+    char const* k = "key";
+    char const* v1 = "value1";
+    mapSet(&map, k, v1);
+
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k), v1);
+    MY_ASSERT_EQUALS(map.count, 1);
+
+    char const* v2 = "value2";
+    mapSet(&map, k, v2);
+
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k), v2);
+    MY_ASSERT_EQUALS(map.count, 1);
+    
+    mapFree(&map);
+}
+
+static void delete_(void) {
+    StringMap map;
+    mapInit(&map, 8);
+    char const* k1 = "key1";
+    char const* v1 = "value1";
+    mapSet(&map, k1, v1);
+    char const* k2 = "key2";
+    char const* v2 = "value2";
+    mapSet(&map, k2, v2);
+    MY_ASSERT_EQUALS(map.count, 2);
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k1), v1);
+    MY_ASSERT_EQUALS(mapDelete(&map, k1), true);
+    MY_ASSERT(mapGet(&map, k1) == NULL);
+    MY_ASSERT_EQUALS(mapDelete(&map, k1), false);
+    MY_ASSERT_EQUALS(map.count, 1);
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k2), v2);
+    MY_ASSERT_EQUALS(mapDelete(&map, k2), true);
+    MY_ASSERT(mapGet(&map, k2) == NULL);
+    MY_ASSERT_EQUALS(mapDelete(&map, k2), false);
+    MY_ASSERT_EQUALS(map.count, 0);
+    mapFree(&map);
+}
+
+static void deletenoinfiniteloop(void) {
+    StringMap map;
+    mapInit(&map, 2);
+    char const* k1 = "key1";
+    char const* v1 = "value1";
+    mapSet(&map, k1, v1);
+    char const* k2 = "key2";
+    char const* v2 = "value2";
+    mapSet(&map, k2, v2);
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k1), v1);
+    MY_ASSERT_EQUALS(mapDelete(&map, k1), true);
+    MY_ASSERT(mapGet(&map, k1) == NULL);
+    MY_ASSERT_EQUALS(mapDelete(&map, k1), false);
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k2), v2);
+    MY_ASSERT_EQUALS(mapDelete(&map, k2), true);
+    MY_ASSERT(mapGet(&map, k1) == NULL);
+    MY_ASSERT_EQUALS(mapDelete(&map, k2), false);
+    char* k3 = strdup("key3");
+    mapGet(&map, k3);
+    free(k3);
+    mapFree(&map);
+}
+
+static void grow(void) {
+    StringMap map;  
+    mapInit(&map, 1);
+    MY_ASSERT_EQUALS(map.capacity, 1);
+    char const* k = "key";
+    char const* v = "value";
+    mapSet(&map, k, v);
+    MY_ASSERT_STRING_EQUALS(mapGet(&map, k), v);
+    MY_ASSERT_EQUALS(map.capacity, 8);
+    mapFree(&map);
+}
+
+static void collision(void) {
+    StringMap map;
+    mapInit(&map, 0);
+    for (char c = 'A'; c < (char)'z'; ++c) {
+        char kbuf[2] = {c, '\0'};
+        char* k = strdup(kbuf);
+        char* v = strdup(kbuf);
+        mapSet(&map, k, v);
+    }
+    for (char c = 'A'; c < (char)'z'; ++c) {
+        char kbuf[2] = {c, '\0'};
+        char* k = kbuf;
+        char const* got = mapGet(&map, k);
+        MY_ASSERT(got != NULL);
+        MY_ASSERT_EQUALS(strlen(got), 1);
+        MY_ASSERT_EQUALS(got[0], c);
+    }
+    mapFree(&map);
+}
+
+int main(void) {
+    basic();
+    overwrite();
+    delete_();
+    deletenoinfiniteloop();
+    grow();
+    collision();
+    return 0;
 }
